5c25c99bb8129538a530cbf97a2ea87fba862641
Matija Topolovec <matija.topolovec@gmail.com>
2020-04-23 16:59:07 +0200

    Implement Sharing icon so that you can share the image using broadcast.

--- app/src/main/java/moj/tvz/hr/listatopolovec/FullSizePictureActivity.java
+++ app/src/main/java/moj/tvz/hr/listatopolovec/FullSizePictureActivity.java
@@ -1,11 +1,17 @@
 package moj.tvz.hr.listatopolovec;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 
@@ -22,5 +28,42 @@ public class FullSizePictureActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.fullSizeImage);
 
         Glide.with(this).asBitmap().load(image).into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.share_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        item.getItemId();
+
+        startAlert(item.getActionView());
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void startAlert(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Do you wana share this image?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = getIntent();
+                        sendBroadcast(intent);
+                        Toast.makeText(getApplicationContext(),"Image shared",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Image not shared", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .create()
+                .show();
+
     }
 }
